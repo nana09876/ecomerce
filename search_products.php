c59c1ac6ba2ad87bf68b89a2056221280baaa49a
Ahmad Maulana (220511138) <136582967+nana09876@users.noreply.github.com>
2024-08-02 10:54:01 +0700

    Update search_products.php

--- search_products.php
+++ search_products.php
@@ -1,5 +1,12 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
 
 // Produk yang tersedia
 $products = [
